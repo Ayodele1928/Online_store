734c17497c231f9209e42eaf88c198769e49dfc8
Ayodele <ayodele.owolabi@gmail.com>
2017-03-28 18:01:38 +0100

    Uploading files and setting file limits and extensions

--- www/test.php
+++ www/test.php
@@ -15,8 +15,49 @@ try{
 
 }
 */
-if (array_key_exists('save', $_POST)){
-	print_r($_FILES);
+
+#max file size..
+define("MAX_FILE_SIZE", "2097152");
+
+#allowed extension..
+$ext = ["image/jpg", "image/jpeg", "image/png"];
+
+if(array_key_exists('save', $_POST)){
+	$errors =[];
+
+	#be sure a file was selected..
+	if(empty($_FILES['pic']['name'])){
+		$errors[] = "Please choose a file";
+	}
+
+	#check file size..
+	if($_FILES['pic']['size'] > MAX_FILE_SIZE){
+		$errors[] = "File size exceeds maximum. maximum: ".MAX_FILE_SIZE;
+	}
+	#check extension
+	if(!in_array($_FILES['pic']['type'], $ext)){
+		$errors[] = "Invalid file type";
+	}
+
+	#generate random number to append
+	$rnd = rand(0000000000, 9999999999);
+
+	#strip filename for spaces
+	$strip_name =str_replace("", "_", $_FILES['pic']['name']);
+
+	$filename = $rnd.$strip_name;
+	$destination = 'uploads/'.$filename;
+
+	if(!move_uploaded_file($_FILES['pic']['tmp_name'], $destination)){
+		$errors[] = "file upload failed";
+	}
+	if(empty($errors)){
+		echo "Done";
+	}else{
+		foreach ($errors as $err) {
+			echo $err. '<br/>';
+		}
+	}
 }
  ?>
   <form id="register" method="POST" enctype="multipart/form-data">
